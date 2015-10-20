ec2ae73ed09dccefe09c5d8a86280d6fb089b67a
daniel <a12danielgd@iessanclemente.net>
2015-10-20 16:43:09 +0200

    second commit

--- app/src/main/java/com/example/daniel/tarefa_i_4_a12danielgd/Tarefa_I_4_a12danielgd.java
+++ app/src/main/java/com/example/daniel/tarefa_i_4_a12danielgd/Tarefa_I_4_a12danielgd.java
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
@@ -38,10 +39,13 @@ public class Tarefa_I_4_a12danielgd extends FragmentActivity {
         comprobarEstadoSD();
         establecerDirectorioFicheiro();
         linhas = new ArrayList<>();
+
     }
 
 
     public void onClickMostrar(View view) {
+        linhas = new ArrayList<>();
+        lerFicheiro();
         FragmentManager fm = getSupportFragmentManager();
         dialogo.show(fm, "");
     }
@@ -76,30 +80,29 @@ public class Tarefa_I_4_a12danielgd extends FragmentActivity {
         EditText etMarca = (EditText) findViewById(R.id.etMarca);
         RadioButton rdbEngadir = (RadioButton) findViewById(R.id.rdbEngadir);
         RadioButton rdbSobreescribir = (RadioButton) findViewById(R.id.rdbSobreescribir);
-        boolean sobrescribir = false;
         Calendar cal = Calendar.getInstance();
-        if (rdbEngadir.isChecked()) sobrescribir = false;
-
-        if (rdbSobreescribir.isChecked()) sobrescribir = true;
-
+        boolean engadir = true;
+        if (rdbEngadir.isChecked()) engadir = true;
+        if (rdbSobreescribir.isChecked()) engadir = false;
         if (sdAccesoEscritura) {
 
             try {
                 if (!etMarca.getText().toString().equals("")) {
-                    OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(rutaCompleta, sobrescribir));
+                    OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(rutaCompleta, engadir));
                     osw.write(etMarca.getText() + " - " + cal.getTime().toString() + "\n");
                     osw.close();
                     Log.i("RUTA COMPLETA", rutaCompleta.getAbsolutePath());
                     Log.i("INTRODUCIDO", etMarca.getText() + " - " + cal.getTime().toString() + "\n");
                     etMarca.setText("");
-                    lerFicheiro();
-                }else Log.e("SD","Campo marca valeiro");
+                } else Log.e("SD", "Campo marca valeiro");
             } catch (Exception ex) {
                 Log.e("SD", "Error escribindo no ficheiro");
-            }
-        } else
-            Toast.makeText(this, "A tarxeta SD non está en modo acceso escritura", Toast.LENGTH_SHORT).show();
 
+            }
+        } else {
+            Toast.makeText(this, "A tarxeta SD non está en modo acceso escritura", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     public void lerFicheiro() {
@@ -113,8 +116,12 @@ public class Tarefa_I_4_a12danielgd extends FragmentActivity {
                     linhas.add(linha);
                 }
                 br.close();
+            } else {
+                Toast.makeText(this, "A tarxeta SD non está en modo acceso lectura", Toast.LENGTH_SHORT).show();
+                finish();
             }
-
+        } catch (IOException ex) {
+            Log.e("ERRRO","Non existe o ficheiro");
         } catch (Exception ex) {
             Toast.makeText(this, "Problemas lendo o ficheiro", Toast.LENGTH_SHORT).show();
             Log.e("INTERNA", "Erro lendo o ficheiro. ");
